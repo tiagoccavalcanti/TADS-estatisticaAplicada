530384ef6d994350305cc1514b7bbf4930c79121
tiago do Carmo Cavalcanti <tiago123.ccavalcanti@gmail.com>
2025-08-18 23:35:47 +0000

    [feat]: create a plot

---
+++ functions/plotly_ts.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+import plotly.express as px
+
+def plot(ticker:str):
+
+    data = yf.download(ticker, period='max', multi_level_index=False)
+    df = data.reset_index()[['Date','Close']]
+
+    fig = px.line(df, x = 'Date', y = 'Close' )
+    return fig
