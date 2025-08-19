b26010b87b93e204b29eaede03c8ab6b73722f65
tiago do Carmo Cavalcanti <tiago123.ccavalcanti@gmail.com>
2025-08-19 03:20:48 +0000

    [feat]: qr Code on main page

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ ticker = st.sidebar.text_input('Escolha o ticker:',  value = 'AAPL')
 fig = plot(ticker)
 
 st.plotly_chart(fig)
+st.image("imagens/frame.png")
 
 # em pyton podesmos usar fstring, nao entendi muito bem oq é, msas funciona colocando um f antes da string (pesquisar)
 # streamlit é bom para pop (prova de conceito) e tambem para minimo produto viavel, no entanto noa é indicado para projetos robustos
